70b91faa47efae1e360c20f8ac3908335cb0b219
AsmaM1983 <149117042+AsmaM1983@users.noreply.github.com>
2023-10-27 13:27:56 +0100

    Add files via upload

--- clustering_app_v3.py
+++ clustering_app_v3.py
@@ -48,52 +48,52 @@ if dataframes:
     dataset = pd.concat(dataframes, ignore_index=True)
     st.write(dataset)
 
-    # Prétraiter les données comme vous l'avez fait précédemment pour calculer recency, Frequency, et Monetary_value
-    #afficher colonne Monetary_value
-    dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
-    dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
+ # Prétraiter les données comme vous l'avez fait précédemment pour calculer recency, Frequency, et Monetary_value
+#afficher colonne Monetary_value
+dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
+dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
 
-    #afficher colonne Frequency
-    dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
-    dataset_fr = dataset_fr.reset_index()
-    dataset_fr.columns = ['CustomerID', 'Frequency']
+ #afficher colonne Frequency
+dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
+dataset_fr = dataset_fr.reset_index()
+dataset_fr.columns = ['CustomerID', 'Frequency']
 
-    # join les deux datatset
-    rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
+ # join les deux datatset
+rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
 
-    #afficher colonne recency
+#afficher colonne recency
 
-    # Convertir la valeur de date maximale en datetime
-    last_max_date = pd.to_datetime('2011-12-09 12:49:00')
-    # Trouver la date maximale parmi les dates d'InvoiceDate
-    dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
-    max_invoice_date =max(dataset['InvoiceDate'])
-    # Sélectionner la date maximale entre les deux
-    max_date = max(last_max_date, max_invoice_date)
+# Convertir la valeur de date maximale en datetime
+last_max_date = pd.to_datetime('2011-12-09 12:49:00')
+# Trouver la date maximale parmi les dates d'InvoiceDate
+dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
+max_invoice_date =max(dataset['InvoiceDate'])
+# Sélectionner la date maximale entre les deux
+max_date = max(last_max_date, max_invoice_date)
 
-    rfm_final['recency'] = (max_date - dataset.groupby('CustomerID')['InvoiceDate'].transform('max')).dt.days
+rfm_final['recency'] = (max_date - dataset.groupby('CustomerID')['InvoiceDate'].transform('max')).dt.days
 
     #Scaling
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-    # Sélectionnez les colonnes à mettre à l'échelle
-    cols_to_scale = ['Monetary_value', 'Frequency', 'recency']
-    # Appliquez la mise à l'échelle aux colonnes sélectionnées
-    rfm_df_scaled= scaler.fit_transform(rfm_final[cols_to_scale])
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+# Sélectionnez les colonnes à mettre à l'échelle
+cols_to_scale = ['Monetary_value', 'Frequency', 'recency']
+# Appliquez la mise à l'échelle aux colonnes sélectionnées
+rfm_df_scaled= scaler.fit_transform(rfm_final[cols_to_scale])
 
-    # Sélectionner les caractéristiques pertinentes pour le clustering
-    X = rfm_df_scaled[['recency', 'Frequency', 'Monetary_value']]
+# Sélectionner les caractéristiques pertinentes pour le clustering
+X = rfm_df_scaled[['recency', 'Frequency', 'Monetary_value']]
 
-    # Charger le modèle K-Means préalablement entraîné
-    k_means=pickle.load(open('model_kmeans.pkl','rb'))
+# Charger le modèle K-Means préalablement entraîné
+k_means=pickle.load(open('model_kmeans.pkl','rb'))
 
     # Attribuer des clusters aux acheteurs
-    clusters = k_means.predict(X)
+clusters = k_means.predict(X)
 
     # Ajouter les étiquettes des clusters attribués en tant que colonne au DataFrame de données
-    dataset['Cluster'] = clusters
+dataset['Cluster'] = clusters
 
     # Afficher le cluster pour chaque CustomerID
-    st.subheader('Clusters attribués aux clients :')
-    for customer_id, cluster in zip(dataset['CustomerID'], dataset['Cluster']):
-        st.write(f"Le cluster du Customer ID = '{customer_id}' est Cluster {cluster}")
+st.subheader('Clusters attribués aux clients :')
+for customer_id, cluster in zip(dataset['CustomerID'], dataset['Cluster']):
+st.write(f"Le cluster du Customer ID = '{customer_id}' est Cluster {cluster}")
