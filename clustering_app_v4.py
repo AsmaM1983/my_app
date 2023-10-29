205582f6bbb0551775e4995defd7fb7b790db79a
AsmaM1983 <149117042+AsmaM1983@users.noreply.github.com>
2023-10-29 02:19:02 +0100

    Add files via upload

--- clustering_app_v4.py
+++ clustering_app_v4.py
@@ -51,26 +51,26 @@ if dataframes:
     if st.button("Exécuter le modèle K-Means"):
         # Prétraiter les données pour calculer recency, Frequency, et Monetary_value
         #afficher colonne Monetary_value
-    	dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
-   	dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
+	dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
+	dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
 
     	#afficher colonne Frequency
-    	dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
-   	dataset_fr = dataset_fr.reset_index()
-    	dataset_fr.columns = ['CustomerID', 'Frequency']
+	dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
+	dataset_fr = dataset_fr.reset_index()
+	dataset_fr.columns = ['CustomerID', 'Frequency']
 
     	# join les deux datatset
-    	rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
+	rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
 
     	#afficher colonne recency
 
     	# Convertir la valeur de date maximale en datetime
-    	last_max_date = pd.to_datetime('2011-12-09 12:49:00')
+	last_max_date = pd.to_datetime('2011-12-09 12:49:00')
     	# Trouver la date maximale parmi les dates d'InvoiceDate
-    	dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
+	dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
     	max_invoice_date =max(dataset['InvoiceDate'])
     	# Sélectionner la date maximale entre les deux
-    	max_date = max(last_max_date, max_invoice_date)
+	max_date = max(last_max_date, max_invoice_date)
 
     	rfm_final['recency'] = (max_date - dataset.groupby('CustomerID')['InvoiceDate'].transform('max')).dt.days
         
