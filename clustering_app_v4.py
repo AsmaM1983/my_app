e1a43e769b9d5f5c7d5d98cc42ebf28341dbcc56
AsmaM1983 <149117042+AsmaM1983@users.noreply.github.com>
2023-10-29 02:06:54 +0100

    Add files via upload

--- clustering_app_v4.py
+++ clustering_app_v4.py
@@ -1,8 +1,8 @@
 
-
 import pickle
 import streamlit as st
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # Titre de l'application
 st.title('Clustering des Clients')
@@ -47,60 +47,55 @@ if dataframes:
     dataset = pd.concat(dataframes, ignore_index=True)
     st.write(dataset)
 
+    # Créer un bouton pour exécuter le modèle K-Means
+    if st.button("Exécuter le modèle K-Means"):
+        # Prétraiter les données pour calculer recency, Frequency, et Monetary_value
+        #afficher colonne Monetary_value
+    	dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
+   	dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
 
-    # Prétraiter les données pour calculer recency, Frequency, et Monetary_value
-    #afficher colonne Monetary_value
-    dataset['Monetary_value']=dataset['Quantity']*dataset['UnitPrice']
-    dataset_m=dataset.groupby('CustomerID')['Monetary_value'].sum()
+    	#afficher colonne Frequency
+    	dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
+   	dataset_fr = dataset_fr.reset_index()
+    	dataset_fr.columns = ['CustomerID', 'Frequency']
 
-    #afficher colonne Frequency
-    dataset_fr=dataset.groupby('CustomerID')['InvoiceNo'].nunique()
-    dataset_fr = dataset_fr.reset_index()
-    dataset_fr.columns = ['CustomerID', 'Frequency']
+    	# join les deux datatset
+    	rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
 
-    # join les deux datatset
-    rfm_final=pd.merge(dataset_m, dataset_fr, on='CustomerID', how='inner')
+    	#afficher colonne recency
 
-    #afficher colonne recency
+    	# Convertir la valeur de date maximale en datetime
+    	last_max_date = pd.to_datetime('2011-12-09 12:49:00')
+    	# Trouver la date maximale parmi les dates d'InvoiceDate
+    	dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
+    	max_invoice_date =max(dataset['InvoiceDate'])
+    	# Sélectionner la date maximale entre les deux
+    	max_date = max(last_max_date, max_invoice_date)
 
-    # Convertir la valeur de date maximale en datetime
-    last_max_date = pd.to_datetime('2011-12-09 12:49:00')
-    # Trouver la date maximale parmi les dates d'InvoiceDate
-    dataset['InvoiceDate'] = pd.to_datetime(dataset['InvoiceDate'], format='%Y-%m-%d %H:%M:%S')
-    max_invoice_date =max(dataset['InvoiceDate'])
-    # Sélectionner la date maximale entre les deux
-    max_date = max(last_max_date, max_invoice_date)
+    	rfm_final['recency'] = (max_date - dataset.groupby('CustomerID')['InvoiceDate'].transform('max')).dt.days
+        
+	# Scaling
+        scaler = MinMaxScaler()
+        cols_to_scale = ['Monetary_value', 'Frequency', 'recency']
+        rfm_df_scaled = scaler.fit_transform(rfm_final[cols_to_scale])
 
-    rfm_final['recency'] = (max_date - dataset.groupby('CustomerID')['InvoiceDate'].transform('max')).dt.days
+        # Convertir rfm_df_scaled en dataframe
+        rfm_df_scaled = pd.DataFrame(rfm_df_scaled)
+        rfm_df_scaled.columns = ['Monetary_value', 'Frequency', 'recency']
 
-    #Scaling
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-    # Sélectionnez les colonnes à mettre à l'échelle
-    cols_to_scale = ['Monetary_value', 'Frequency', 'recency']
-    # Appliquez la mise à l'échelle aux colonnes sélectionnées
-    rfm_df_scaled= scaler.fit_transform(rfm_final[cols_to_scale])
+        # Sélectionner les caractéristiques pertinentes pour le clustering
+        X = rfm_df_scaled[['recency', 'Frequency', 'Monetary_value']]
 
-    # convertir rfm_df_scaled en dataframe
-    rfm_df_scaled = pd.DataFrame(rfm_df_scaled)
-    rfm_df_scaled.columns = ['Monetary_value', 'Frequency', 'recency']
+        # Charger le modèle K-Means préalablement entraîné
+        k_means = pickle.load(open('model_kmeans.pkl', 'rb'))
 
-    # Sélectionner les caractéristiques pertinentes pour le clustering
-    X = rfm_df_scaled[['recency', 'Frequency', 'Monetary_value']]
+        # Attribuer des clusters aux acheteurs
+        clusters = k_means.predict(X)
 
-# Créer un bouton pour exécuter le modèle K-Means
-if st.button("Exécuter le modèle K-Means"):
-    
-# Charger le modèle K-Means préalablement entraîné
-    k_means=pickle.load(open('model_kmeans.pkl','rb'))
+        # Ajouter les étiquettes des clusters attribués en tant que colonne au DataFrame de données
+        rfm_final['Cluster'] = clusters
 
-    # Attribuer des clusters aux acheteurs
-    clusters = k_means.predict(X)
-
-    # Ajouter les étiquettes des clusters attribués en tant que colonne au DataFrame de données
-    rfm_final['Cluster'] = clusters
-
-# Afficher le cluster pour chaque CustomerID
-    st.subheader('Clusters attribués aux clients :')
-    for customer_id, cluster in zip(rfm_final['CustomerID'], rfm_final['Cluster']):
-        st.write(f"Le cluster du Customer ID = '{customer_id}' est Cluster {cluster}")
+        # Afficher le cluster pour chaque CustomerID
+        st.subheader('Clusters attribués aux clients :')
+        for customer_id, cluster in zip(rfm_final['CustomerID'], rfm_final['Cluster']):
+            st.write(f"Le cluster du Customer ID = '{customer_id}' est Cluster {cluster}")
