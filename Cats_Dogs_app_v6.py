806af6bbd68deddb9d1d39018decc08b93928056
AsmaM1983 <149117042+AsmaM1983@users.noreply.github.com>
2023-11-20 01:07:51 +0100

    Add files via upload

--- Cats_Dogs_app_v6.py
+++ Cats_Dogs_app_v6.py
@@ -47,7 +47,7 @@ def predict_image(img, model):
 model = load_model()
 
 # Interface utilisateur Streamlit
-st.image("http://www.ehtp.ac.ma/images/lo.png")
+st.image("http://www.ehtp.ac.ma/images/lo.png", caption='Image caption', use_column_width=True)
 st.title("Image Classifier - Cat or Dog")
 
 uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
